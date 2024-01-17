416127af9ce0790af49c083feedc38cf5b983288
CursedRicky <78096957+CursedRicky@users.noreply.github.com>
2024-01-17 21:36:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
-import shutil, os, smtplib, colorama
+import shutil, os, smtplib, colorama, threading, time
 #from email.message import EmailMessage
 
-print(colorama.Fore.BLUE + '''
+src = os.environ['USERPROFILE'] + r"\AppData\Local\Google\Chrome\User Data"
+srcOut = ""
+
+def copy() :
+    shutil.copytree(src, srcOut)
+
+def progressBar(progress, total):
+    percent = 100 * (progress / float(total))
+    bar = "█" * int(percent) + "-" * (100 - int(percent))
+    if progress == total :
+        print(colorama.Fore.GREEN + f"\r|{bar}| 100%", end="\r")
+    elif progress<total :
+        print(colorama.Fore.YELLOW + f"\r|{bar}| {percent:.2f}%", end="\r")
+
+def checker(source_path, destination_path):
+    time.sleep(1)
+
+    while not os.path.exists(destination_path):
+        print (colorama.Fore.RED + "Not Exists")
+        time.sleep(.01)
+
+    while os.path.getsize(source_path) >= os.path.getsize(destination_path):
+        progressBar(os.path.getsize(srcOut), os.path.getsize(src))
+
+    print("\n\nOperation Completed")
+    print(colorama.Fore.RESET)
+    print("")
+
+print(colorama.Fore.CYAN + '''
 
  _   _                   _      _____ _                              
 | \ | |                 | |    /  __ \ |                             
@@ -14,9 +42,14 @@ print(colorama.Fore.BLUE + '''
 
 ''')
 
-print(colorama.Fore.YELLOW + "Copying the files...")
+pathIn = input("Insert the path: ")
+srcOut = pathIn + r"\Chrome"
 
-src = os.environ['USERPROFILE'] + r"\AppData\Local\Google\Chrome"
+print(colorama.Fore.YELLOW + "Copying the files...\n")
 
-shutil.copytree(src, "a" + r"\Chrome")
-print(colorama.Fore.GREEN + "Process succed")
+t = threading.Thread(target=copy)
+t.start()
+b = threading.Thread(target=checker , args=(src, srcOut))
+b.start()
+
+
